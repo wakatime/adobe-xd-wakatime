f4658ab2e1f8d23a6d7a0823561a8d441df86197
Alan Hamlett <alan.hamlett@gmail.com>
2022-06-24 14:40:49 -0300

    prettier

--- main.js
+++ main.js
@@ -11,64 +11,64 @@ author:      @jvelezpo
 
 let application = require('application');
 const { shell } = require('uxp');
-let scenegraph = require("scenegraph");
+let scenegraph = require('scenegraph');
 const os = require('os');
 
 const Preferences = require('./lib/preferences');
 const Libs = require('./lib/libs');
-const { prompt, errorOpenWakatime } = require("./lib/dialog/dialogs");
+const { prompt, errorOpenWakatime } = require('./lib/dialog/dialogs');
 
 const VERSION = '1.0.0';
 let lastAction = 0,
-    lastFile = undefined,
-    lastGUID = undefined;
+  lastFile = undefined,
+  lastGUID = undefined;
 
 const openDashboardWebsite = async () => {
-    const error = await errorOpenWakatime('Error', validation);
-    if (error.which === 0) {
-        const url = 'https://wakatime.com/';
-        shell.openExternal(url)
-    }
+  const error = await errorOpenWakatime('Error', validation);
+  if (error.which === 0) {
+    const url = 'https://wakatime.com/';
+    shell.openExternal(url);
+  }
 };
 
 const getApiKey = async () => {
-    const preferences = await Preferences.createFromSettings();
-    const validation = Libs.validateKey(preferences.apiKey);
-    if (validation !== '') {
-        return '';
-    }
-    return preferences.apiKey;
-}
+  const preferences = await Preferences.createFromSettings();
+  const validation = Libs.validateKey(preferences.apiKey);
+  if (validation !== '') {
+    return '';
+  }
+  return preferences.apiKey;
+};
 
 const enoughTimePassed = () => {
-    return lastAction + 120000 < Date.now();
-}
+  return lastAction + 120000 < Date.now();
+};
 
 const sendHeartbeat = async (file, time, project, language, isWrite, lines) => {
-    const apiKey = await getApiKey();
-    try {
-        await fetch('https://wakatime.com/api/v1/heartbeats', {
-            method: 'POST',
-            body: JSON.stringify({
-                time: time / 1000,
-                entity: file,
-                type: 'app',
-                project,
-                language,
-                is_write: isWrite ? true : false,
-                lines: lines,
-                plugin: 'adobexd-wakatime/' + VERSION + ' ' + os.platform(),
-            }),
-            headers: {
-                'Authorization': 'Basic ' + Libs.btoa(apiKey),
-                'Content-Type': 'application/json'
-            }
-        });
-    } catch (err) {
-        console.log('Error(heartbeats):', err);
-    }
-    lastAction = time;
-    lastFile = file;
+  const apiKey = await getApiKey();
+  try {
+    await fetch('https://wakatime.com/api/v1/heartbeats', {
+      method: 'POST',
+      body: JSON.stringify({
+        time: time / 1000,
+        entity: file,
+        type: 'app',
+        project,
+        language,
+        is_write: isWrite ? true : false,
+        lines: lines,
+        plugin: 'adobexd-wakatime/' + VERSION + ' ' + os.platform(),
+      }),
+      headers: {
+        Authorization: 'Basic ' + Libs.btoa(apiKey),
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (err) {
+    console.log('Error(heartbeats):', err);
+  }
+  lastAction = time;
+  lastFile = file;
 };
 
 const handleAction = (isWrite) => {
@@ -82,44 +82,41 @@ const handleAction = (isWrite) => {
       sendHeartbeat(currentDocument.name, time, currentDocument.name, undefined, isWrite, null);
     }
   }
-}
+};
 
 (() => {
-    console.log("Initializing WakaTime plugin v" + VERSION);
-    window.setInterval(async () => {
-        handleAction(false);
-    }, 5000);
-}
-)();
+  console.log('Initializing WakaTime plugin v' + VERSION);
+  window.setInterval(async () => {
+    handleAction(false);
+  }, 5000);
+})();
 
 /**
  * Create the input modal where the user inputs his/her api key
  */
 const openApiKeyDialog = async () => {
+  const preferences = await Preferences.createFromSettings();
 
-    const preferences = await Preferences.createFromSettings();
+  const title = 'WakaTime';
+  const message = 'Enter your wakatime.com api key:';
 
-    const title = 'WakaTime';
-    const message = 'Enter your wakatime.com api key:';
+  const result = await prompt(title, message, 'api key', preferences.apiKey);
 
-    const result = await prompt(title, message, 'api key', preferences.apiKey);
+  if (result.which === 1) {
+    const newApiKey = result.value;
+    preferences.apiKey = newApiKey;
+    const validation = Libs.validateKey(newApiKey);
 
-    if (result.which === 1) {
-
-        const newApiKey = result.value;
-        preferences.apiKey = newApiKey;
-        const validation = Libs.validateKey(newApiKey)
-
-        if (validation !== '') {
-            openDashboardWebsite();
-        } else {
-            await prompt(title, 'api key set correctly');
-        }
+    if (validation !== '') {
+      openDashboardWebsite();
+    } else {
+      await prompt(title, 'api key set correctly');
     }
+  }
 };
 
 module.exports = {
-    commands: {
-        apiKeyDialog: openApiKeyDialog
-    }
+  commands: {
+    apiKeyDialog: openApiKeyDialog,
+  },
 };
